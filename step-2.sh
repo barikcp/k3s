e5e93ed83234d37eded8ef08d3ae2ac3ef080fc2
barikcp <113718872+barikcp@users.noreply.github.com>
2022-09-19 01:16:56 +0530

    Create step-2.sh

---
+++ step-2.sh
@@ -0,0 +1,5 @@
+# Execute these command in worker-node-1
+wget https://github.com/k3s-io/k3s/releases/download/v1.23.5%2Bk3s1/k3s                # Download the binary
+chmod +x k3s                                                                           # Give Execute permission
+sudo ./k3s agent --server https://k3s-master-node:6443 --token ${NODE_TOKEN}           # Replace the NODE_TOKEN name, with the output of command fired in Master Node: cat /var/lib/rancher/k3s/server/token
+sudo ./k3s kubectl get nodes -o wide                                                   # Get how many nodes created
